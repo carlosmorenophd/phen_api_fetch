f47784571f35cc6170e13b7b9f4a1533476a87c5
Juan Carlos Moreno Sanchez <carlos.moreno.phd@gmail.com>
2023-12-01 22:46:55 -0600

    Create a new env to store the ftp parameters

--- app/ftp_client/FtpTransfer.py
+++ app/ftp_client/FtpTransfer.py
@@ -1,29 +1,42 @@
 from datetime import datetime
 import ftplib
-import os
+from os import getenv
+from dotenv import load_dotenv
+
 
 class FtpTransfer ():
-    def __init__(self, hostname: str, port: int,  username: str, password: str) -> None:
-        self.hostname = hostname
-        self.username = username
-        self.password = password
-        self.port = port
+    def __init__(self, hostname: str = "", port: int = 0,  username: str = "", password: str = "") -> None:
+        load_dotenv()
+        if hostname != "":
+            self.hostname = hostname
+        else:
+            self.hostname = getenv("FTP_HOSTNAME")
+        if port != 0:
+            self.port = port
+        else:
+            self.port = int(getenv("FTP_PORT"))
+        if username != "":
+            self.username = username
+        else:
+            self.username = getenv("FTP_USERNAME")
+        if password != "":
+            self.password = password
+        else:
+            self.password = getenv("FTP_PASSWORD")
 
     def transfer_csv(self, local_file: str):
         session = ftplib.FTP()
         date = datetime.now()
 
-
-        remote_file = local_file.replace(
-            ".csv", "_{}.csv".format(date.strftime("%H%M")))
+        remote_file = "{}_{}".format(date.strftime("%H%M"), local_file)
         session.connect(host=self.hostname, port=self.port)
         session.login(user=self.username, passwd=self.password)
-        folderName = date.strftime("%Y%m%d").replace("/","")
+        folderName = date.strftime("%Y%m%d").replace("/", "")
         if not folderName in session.nlst():
             session.mkd(folderName)
         session.cwd(folderName)
         with open(local_file, 'rb') as fp:
-            session.storbinary("STOR %s"%remote_file, fp)
+            session.storbinary("STOR %s" % remote_file, fp)
         session.quit()
 
 
