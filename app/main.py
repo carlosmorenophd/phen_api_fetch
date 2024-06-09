3cde69140e8b2c181beaad540284d18a4146621c
Juan Carlos Moreno Sanchez <carlos.moreno.phd@gmail.com>
2024-06-09 02:28:40 +0000

    Adding new file to split add and remove on main

--- app/main.py
+++ app/main.py
@@ -1,30 +1,5 @@
-from fastapi import FastAPI
-from fastapi_pagination import add_pagination
 import uvicorn
-
-import database
-from routes import datasetRoute
-from dependencies import get_db
-
-
-database.db.connect()
-database.db.close()
-
-app = FastAPI()
-sleep_time = 10
-
-version_one = FastAPI()
-version_one.include_router(datasetRoute.router)
-add_pagination(version_one)
-
-app.mount("/api/v1", version_one)
 
 
 if __name__ == "__main__":
-    uvicorn.run(
-        app,
-        host="0.0.0.0",
-        port=8000,
-        reload_dirs=["app"],
-        log_level="debug",
-    )
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
